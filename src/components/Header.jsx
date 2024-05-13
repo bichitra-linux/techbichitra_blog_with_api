5f337fb2e4a168eb0ad576d9eefeb9f02ebbdacd
Bichitra Gautam <bichitragautam25@gmail.com>
2024-05-13 21:11:05 +0545

    chore: Update Header component layout and link styles
    This commit updates the Header component layout by adding a new div container to group the search form and search button. It also improves the link styles by adding the "text-inherit" and "no-underline" classes to the Link components. This change enhances the user experience by providing a more organized layout and improving the visual appearance of the links in the header.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -116,14 +116,14 @@ export default function Header() {
         <Navbar.Toggle />
       </div>
       <Navbar.Collapse>
-        <Navbar.Link active={path === '/'} as={'div'}>
-          <Link to='/'>Home</Link>
+        <Navbar.Link active={path === '/'} as={'div'} className="mx-2">
+          <Link to='/' className="text-inherit no-underline">Home</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/about'} as={'div'}>
-          <Link to='/about'>About</Link>
+        <Navbar.Link active={path === '/about'} as={'div'} className="mx-2">
+          <Link to='/about' className="text-inherit no-underline">About</Link>
         </Navbar.Link>
-        <Navbar.Link active={path === '/projects'} as={'div'}>
-          <Link to='/projects'>Projects</Link>
+        <Navbar.Link active={path === '/projects'} as={'div'} className="mx-2">
+          <Link to='/projects' className="text-inherit no-underline">Projects</Link>
         </Navbar.Link>
       </Navbar.Collapse>
     </Navbar>
